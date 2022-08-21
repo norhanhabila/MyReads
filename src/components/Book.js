3f61a5f510f393c1ba6aedebf89dcc944c566769
norhanhabila <norhan.habila@gmail.com>
2022-08-21 17:04:49 +0200

    error fixed

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,12 +2,16 @@ import React from "react";
 import BookCover from "./BookCover";
 import BookShelfChanger from "./BookShelfChanger";
 
-const Book = ({ book, changeBookShelf }) => {
+const Book = ({ shelf, book, changeBookShelf }) => {
   return (
     <div className="book">
       <div className="book-top">
         <BookCover book={book} />
-        <BookShelfChanger book={book} changeBookShelf={changeBookShelf} />
+        <BookShelfChanger
+          shelf={shelf}
+          book={book}
+          changeBookShelf={changeBookShelf}
+        />
       </div>
       <div className="book-title">{book.title}</div>
       <div className="book-authors">{book.authors}</div>
